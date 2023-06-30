3a5277b97e3b3aaf04f7ba171824ed884e8de472
HieuT23511 <hieut23511@gmail.com>
2023-06-30 10:20:56 +0700

    push all option

---
+++ src/routers/web.routers.ts
@@ -0,0 +1,14 @@
+import {Router} from "express";
+import {StudentControllers} from "../controllers/student.controllers";
+
+export const webRouters = Router();
+
+webRouters.get("/create",StudentControllers.getCreateNewStudentPage);
+webRouters.post("/create",StudentControllers.createNewStudent);
+webRouters.get("/list",StudentControllers.getListStudentsPage);
+webRouters.post("/list",StudentControllers.getListStudentsPage);
+webRouters.get("/:id/detail/",StudentControllers.getDetailStudentPage)
+webRouters.get("/:id/update/",StudentControllers.getUpdateStudentPage);
+webRouters.post("/:id/update/",StudentControllers.updateStudent);
+webRouters.get("/:id/delete/",StudentControllers.deleteStudent);
+
